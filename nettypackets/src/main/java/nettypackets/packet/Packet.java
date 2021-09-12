b448d539684a0201487267fa48c87c09f5302028
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2021-09-12 12:51:51 -0700

    removed test

--- nettypackets/src/main/java/nettypackets/packet/Packet.java
+++ nettypackets/src/main/java/nettypackets/packet/Packet.java
@@ -7,9 +7,7 @@ public abstract class Packet {
 
     public Packet(){}
 
-    public void writeBytes(ByteBuf out){
-        System.out.println("test #3");
-    }
+    public void writeBytes(ByteBuf out){ }
 
     public void readBytes(ByteBuf in){  }
 
