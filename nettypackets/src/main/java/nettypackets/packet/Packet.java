bf38fe24e26c01a25b932a7812bc417e9c0fe3ee
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2021-09-12 12:49:41 -0700

    test 3

--- nettypackets/src/main/java/nettypackets/packet/Packet.java
+++ nettypackets/src/main/java/nettypackets/packet/Packet.java
@@ -8,7 +8,7 @@ public abstract class Packet {
     public Packet(){}
 
     public void writeBytes(ByteBuf out){
-        System.out.println("test #2");
+        System.out.println("test #3");
     }
 
     public void readBytes(ByteBuf in){  }
